17159e63a9658b6ec045e2269edc307cb496a35b
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2023-07-29 22:17:46 +0530

    Add files via upload

---
+++ labsheet5question4/labsheet5question4/Program.cs
@@ -0,0 +1,33 @@
+﻿using labsheet5question4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet5question4
+{
+    class SeparateClass
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+// Program.cs file
+
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeparateClass obj = new SeparateClass();
+            obj.sayHello(); // This will work now
+            Console.ReadLine();
+        }
+    }
+}
