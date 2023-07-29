899cb057ed668c9d2194797ac4609e579a48d340
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2023-07-29 20:35:45 +0530

    Add files via upload

---
+++ lab 4/labsheet4question1(3)/labsheet4question1(3)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet4question1_3_
+{
+    
+    
+        class ConvertValues
+        {
+            public double kilometerTOmeter(double km)
+            {
+                double m = km * 1000;
+                return m;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                ConvertValues converter = new ConvertValues();
+                Console.Write("Enter a distance in kilometers: ");
+                double km = double.Parse(Console.ReadLine());
+                double m = converter.kilometerTOmeter(km);
+                Console.WriteLine(km + " kilometers is equal to " + m + " meters.");
+                Console.ReadLine();
+            }
+        }
+    }
---
+++ lab 4/labsheet4question2(1)/labsheet4question2(1)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet4question2_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area, circumference;
+            const double pi = 3.14159;
+
+            Console.Write("Enter the radius of the circle: ");
+            radius = double.Parse(Console.ReadLine());
+
+            area = pi * radius * radius;
+            circumference = 2 * pi * radius;
+
+            Console.WriteLine("The area of the circle is: " + area);
+            Console.WriteLine("The circumference of the circle is: " + circumference);
+            Console.ReadLine();
+        }
+    }
+}
+
