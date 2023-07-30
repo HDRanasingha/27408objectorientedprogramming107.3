f4b4fd1533962964e15a95039119fc3c1b2b32e3
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2023-07-30 10:09:13 +0530

    Add files via upload

---
+++ 1lab1quiz/1lab1quiz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1lab1quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my batch is 22.2");
+            Console.WriteLine("my name is hashini");
+            Console.ReadLine();
+        }
+    }
+}
